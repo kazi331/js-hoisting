76a1584d78455e2eb0c314854c901682a8cd21d5
kazi331 <kazisharifulislam52@gmail.com>
2022-06-28 20:40:03 +0600

    hoisting

--- hoisting2.js
+++ hoisting2.js
@@ -15,21 +15,39 @@ hoisting means to lift up something.
 
    // variable hoisting --------
    // initialization and declaration ( initializations are not hoisted )
-   
+   /* console.log(x);                                
+   x = 'Mr. X'
+   var x; */
 
    // variables inside of function
-   
+   // console.log(me)
+   /* function callMe(){
+      b = 'someone'
+      console.log(b);
+      console.log(me)
+      var me = "Sayem";
+      var b;
+   }
+   callMe(); */
 
    // let, const (let and const are hoisted, but...)
-  
+   // console.log(name1)
+   // let name1 = 'My name is john'
+   // const name2 = 'Hello John'
 
 
 
    // function hoisting
-  
+   /* age();
+   function age(){
+      console.log(23)
+   } */
 
    // function expression (expression doesn't hoisted)
- 
+ /*   sayHello();
+   let sayHello = function(){
+      console.log("Hello Bangladesh!!")
+   } */
 
    // important note: 
    /* hoisting is not available in c, c++, java, python.
